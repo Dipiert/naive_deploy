36349535bef59d99ea11a20fc2e360bac7a9784e
dipiert <dipiert@gmail.com>
2018-05-23 11:54:52 -0300

    reduce hardcode

--- local.sh
+++ local.sh
@@ -3,6 +3,10 @@ WEB_HOME="/var/www/html/"
 APP_NAME="hojaruta" 
 AUX_SUFFIX="_deploy"
 APP_HOME=$WEB_HOME$APP_NAME
+SSH_USER="soporte"
+SSH_HOST="koha.parquedelconocimiento.com"
+SSH_PATH="/tmp/"
+REMOTE=$SSH_USER@$SSH_HOST:$SSH_PATH
 DEPLOY_FOLDER=$APP_NAME$AUX_SUFFIX
 
 cp -R  $APP_HOME $APP_HOME$AUX_SUFFIX
--- remote.sh
+++ remote.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
+SSH_PATH="/tmp/"
+WEB_HOME="/var/www/html/"
+APP_NAME="hojaruta"
+AUX_SUFFIX="_deploy"
+DEPLOY_FOLDER=$APP_NAME$AUX_SUFFIX
 
-cd /tmp/
-cp hojaruta_deploy/. /var/www/html/hojaruta/
+cd $SSH_PATH
+cp -r $DEPLOY_FOLDER/. $WEB_HOME$APP_NAME && rm -r $DEPLOY_FOLDER
+
