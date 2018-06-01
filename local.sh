357e2b6e10a9d42afb2c9e187eb133125593d88e
Damián Rotta <dipiert@gmail.com>
2018-06-01 10:26:19 -0300

    [FIX] typo: prefix <- suffix

--- local.sh
+++ local.sh
@@ -10,6 +10,6 @@ REMOTE=$SSH_USER@$SSH_HOST:$SSH_PATH
 DEPLOY_FOLDER=$APP_NAME$AUX_SUFFIX
 
 cp -R  $APP_HOME $APP_HOME$AUX_SUFFIX
-cd $APP_HOME$AUX_PREFIX && rm -rf .git .idea/ .gitignore sql/
+cd $APP_HOME$AUX_SUFFIX && rm -rf .git .idea/ .gitignore sql/
 cd .. && scp -r $DEPLOY_FOLDER $REMOTE
 rm -rf $DEPLOY_FOLDER
